4a4d703e45c37fc7fea443f05b67324447a54867
aidma12 <aidma12@aidma-training>
2014-05-27 10:03:31 +0900

    これは初めてのコミットです

---
+++ MavenDemo/src/main/java/com/hubpages/plusminus/MavenDemo.java
@@ -0,0 +1,34 @@
+package com.hubpages.plusminus;
+ 
+import com.opensymphony.xwork2.ActionSupport;
+ 
+public class MavenDemo extends ActionSupport{
+ 
+    private static final long serialVersionUID = 1L;
+ 
+    public String message = "Hello PlusMinus....";
+     
+    public String getMessage() {
+        return message;
+    }
+ 
+    public void setMessage(String message) {
+        this.message = message;
+    }
+ 
+    public String displayMessage(){
+        setMessage(message);
+        System.out.println(message);
+        return SUCCESS;
+    }
+     
+    public String executeAll(){
+        displayMessage();
+        return SUCCESS;
+    }
+     
+    public static void main (String args[]){
+        MavenDemo md = new MavenDemo();
+        md.executeAll();
+    }
+}
